0be41f938d9f11f792607eff48f7346ed424baa7
mourad zamoun <mourad.zamoun@gmail.com>
2021-05-25 16:58:07 +0200

    login ok

--- gen.js
+++ gen.js
@@ -1,7 +1,21 @@
 //////////////////
 function loadDiv(idDiv, html) {
-   $(idDiv).html(html);
+    var el = $('#'+idDiv);
+    if(el) {
+        el.html(html);
+    }
 }
 
 ///////////////////////
 
+function showDiv(idDiv, ok) {
+    // style="display:none;
+    var el = $('#'+idDiv);
+    if(el) {
+        if(ok) {
+            el.show();
+        }else {
+            el.hide();
+        }
+    }
+}
