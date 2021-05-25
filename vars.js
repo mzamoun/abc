219f6fb627e03e7287068873afc3d1a006d2c6c5
mourad zamoun <mourad.zamoun@gmail.com>
2021-05-25 23:51:06 +0200

    login forgot ok

--- vars.js
+++ vars.js
@@ -1,1 +1,1 @@
-var url = 'https://script.google.com/macros/s/AKfycbz9Ir8gTZ763PbNqam7pFZ3HXUTLDxkRe4I4-gw0_wWxhJ47Pk592pduJj8xYm8LnmC/exec';
+var url = 'https://script.google.com/macros/s/AKfycbwSRyVjiLv1eaWdRSQ0Ly9cHYDTulP8pD1H3VJH_tvJ_sRHWYWznCOGG4rWueYZi5di/exec';
